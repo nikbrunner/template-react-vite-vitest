1668308dac0a4b67e74fa378169ac26bf8bd86cc
nbr <nikolaus.brunner@protonmail.ch>
2026-01-28 19:27:44 +0100

    refactor: remove commented solutions from test scaffolding

--- src/components/PasswordStrength/PasswordStrength.test.tsx
+++ src/components/PasswordStrength/PasswordStrength.test.tsx
@@ -6,42 +6,22 @@ import { PasswordStrength } from "./PasswordStrength";
 
 describe("PasswordStrength", () => {
     test("renders input field", () => {
-        // TODO: Implement
-        // render(<PasswordStrength />);
-        // expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+        // TODO
     });
 
     test("shows Weak for short passwords (< 6 chars)", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<PasswordStrength />);
-        // await user.type(screen.getByLabelText(/password/i), "abc");
-        // expect(screen.getByText(/weak/i)).toBeInTheDocument();
+        // TODO
     });
 
     test("shows Medium for alphanumeric passwords (6+ chars)", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<PasswordStrength />);
-        // await user.type(screen.getByLabelText(/password/i), "abc123");
-        // expect(screen.getByText(/medium/i)).toBeInTheDocument();
+        // TODO
     });
 
     test("shows Strong for complex passwords (8+ chars with special)", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<PasswordStrength />);
-        // await user.type(screen.getByLabelText(/password/i), "abc123!@");
-        // expect(screen.getByText(/strong/i)).toBeInTheDocument();
+        // TODO
     });
 
     test("show/hide toggle works correctly", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<PasswordStrength />);
-        // const input = screen.getByLabelText(/password/i);
-        // expect(input).toHaveAttribute("type", "password");
-        // await user.click(screen.getByRole("button", { name: /show/i }));
-        // expect(input).toHaveAttribute("type", "text");
+        // TODO
     });
 });
