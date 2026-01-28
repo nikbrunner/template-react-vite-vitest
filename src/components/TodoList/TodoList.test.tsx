968dcde589ba94606902d5fcaa85ad89b4648ea4
nbr <nikolaus.brunner@protonmail.ch>
2026-01-28 19:28:04 +0100

    refactor: remove commented solutions from test scaffolding

--- src/components/TodoList/TodoList.test.tsx
+++ src/components/TodoList/TodoList.test.tsx
@@ -6,41 +6,22 @@ import { TodoList } from "./TodoList";
 
 describe("TodoList", () => {
     test("can add a new todo", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<TodoList />);
-        // const input = screen.getByRole("textbox");
-        // await user.type(input, "New todo");
-        // await user.click(screen.getByRole("button", { name: /add/i }));
-        // expect(screen.getByText("New todo")).toBeInTheDocument();
+        // TODO
     });
 
     test("can toggle todo completion", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<TodoList />);
-        // // Add a todo first, then toggle it
+        // TODO
     });
 
     test("can delete a todo", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<TodoList />);
-        // // Add a todo first, then delete it
+        // TODO
     });
 
     test("filters work correctly", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<TodoList />);
-        // // Add todos with different completion states
-        // // Click filter buttons and verify visibility
+        // TODO
     });
 
     test("counter shows correct number", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<TodoList />);
-        // // Add todos and verify counter text
+        // TODO
     });
 });
