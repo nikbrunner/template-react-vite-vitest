e03070ad88c4b0334141ec8ff93ced6727bf4e6e
nbr <nikolaus.brunner@protonmail.ch>
2026-01-28 19:30:51 +0100

    refactor: remove commented solutions from test scaffolding

--- src/components/DataTable/DataTable.test.tsx
+++ src/components/DataTable/DataTable.test.tsx
@@ -20,49 +20,26 @@ function TestWrapper({ children }: { children: React.ReactNode }) {
 
 describe("DataTable", () => {
     test("shows loading state while fetching", () => {
-        // TODO: Implement
-        // render(<DataTable />, { wrapper: TestWrapper });
-        // expect(screen.getByText(/loading/i)).toBeInTheDocument();
+        // TODO
     });
 
     test("renders data in table format", async () => {
-        // TODO: Implement
-        // render(<DataTable />, { wrapper: TestWrapper });
-        // expect(await screen.findByText("Alice Johnson")).toBeInTheDocument();
-        // expect(screen.getByText("alice@example.com")).toBeInTheDocument();
+        // TODO
     });
 
     test("sorts by column on header click", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<DataTable />, { wrapper: TestWrapper });
-        // await screen.findByText("Alice Johnson");
-        // await user.click(screen.getByText("Name"));
-        // // Verify sort order changed
+        // TODO
     });
 
     test("reverses sort on second click", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<DataTable />, { wrapper: TestWrapper });
-        // await screen.findByText("Alice Johnson");
-        // await user.click(screen.getByText("Name"));
-        // await user.click(screen.getByText("Name"));
-        // // Verify sort order reversed
+        // TODO
     });
 
     test("pagination controls work correctly", async () => {
-        // TODO: Implement
-        // const user = userEvent.setup();
-        // render(<DataTable />, { wrapper: TestWrapper });
-        // await screen.findByText("Page 1 of 3");
-        // await user.click(screen.getByRole("button", { name: /next/i }));
-        // expect(screen.getByText("Page 2 of 3")).toBeInTheDocument();
+        // TODO
     });
 
     test("shows correct page info", async () => {
-        // TODO: Implement
-        // render(<DataTable />, { wrapper: TestWrapper });
-        // expect(await screen.findByText(/page 1 of/i)).toBeInTheDocument();
+        // TODO
     });
 });
