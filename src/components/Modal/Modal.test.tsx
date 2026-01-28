1227ebc225aaca37874c62639877d909319e4c32
nbr <nikolaus.brunner@protonmail.ch>
2026-01-28 19:29:19 +0100

    refactor: remove commented solutions from test scaffolding

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -6,50 +6,26 @@ import { Modal } from "./Modal";
 
 describe("Modal", () => {
     test("does not render when closed", () => {
-        // TODO: Implement
-        // render(<Modal isOpen={false} onClose={vi.fn()} title="Test">Content</Modal>);
-        // expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+        // TODO
     });
 
     test("renders when open", () => {
-        // TODO: Implement
-        // render(<Modal isOpen={true} onClose={vi.fn()} title="Test">Content</Modal>);
-        // expect(screen.getByRole("dialog")).toBeInTheDocument();
+        // TODO
     });
 
     test("closes on Escape key press", async () => {
-        // TODO: Implement
-        // const onClose = vi.fn();
-        // const user = userEvent.setup();
-        // render(<Modal isOpen={true} onClose={onClose} title="Test">Content</Modal>);
-        // await user.keyboard("{Escape}");
-        // expect(onClose).toHaveBeenCalled();
+        // TODO
     });
 
     test("closes on overlay click", async () => {
-        // TODO: Implement
-        // const onClose = vi.fn();
-        // const user = userEvent.setup();
-        // render(<Modal isOpen={true} onClose={onClose} title="Test">Content</Modal>);
-        // Click on overlay (parent of modal)
-        // await user.click(screen.getByRole("dialog").parentElement!);
-        // expect(onClose).toHaveBeenCalled();
+        // TODO
     });
 
     test("does NOT close when clicking modal content", async () => {
-        // TODO: Implement
-        // const onClose = vi.fn();
-        // const user = userEvent.setup();
-        // render(<Modal isOpen={true} onClose={onClose} title="Test">Content</Modal>);
-        // await user.click(screen.getByText("Content"));
-        // expect(onClose).not.toHaveBeenCalled();
+        // TODO
     });
 
     test("has correct accessibility attributes", () => {
-        // TODO: Implement
-        // render(<Modal isOpen={true} onClose={vi.fn()} title="Test Modal">Content</Modal>);
-        // const dialog = screen.getByRole("dialog");
-        // expect(dialog).toHaveAttribute("aria-modal", "true");
-        // expect(dialog).toHaveAttribute("aria-labelledby", "modal-title");
+        // TODO
     });
 });
